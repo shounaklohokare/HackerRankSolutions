00376e584928bf1af9950c0dc156d79bc7410d53
Shounak Lohokare <41364885+shounaklohokare@users.noreply.github.com>
2020-10-04 22:09:07 +0530

    Add files via upload

---
+++ HackerRank Picking Numbers Solution.py
@@ -0,0 +1,14 @@
+from collections import Counter
+def pickingNumbers(a):
+    arr = Counter(a)
+    maxnum=0
+    print(arr)
+    for i in range(1, 100):
+        print(f"for {i} maxnum is {maxnum} and arr[i] is {arr[i]} and arr[i+1] is {arr[i+1]}")
+        maxnum = max(maxnum, arr[i+1]+arr[i])
+        
+    return maxnum
+
+    
+
+pickingNumbers([4, 6, 5, 3, 3, 1])                
