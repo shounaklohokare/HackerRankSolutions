5eca9f09777901c68061bd93d19efd02377e4c7a
Shounak Lohokare <41364885+shounaklohokare@users.noreply.github.com>
2020-11-22 19:05:25 +0530

    Delete HackerRank Equalize the Array.py

--- HackerRank Equalize the Array.py
+++
@@ -1,6 +0,0 @@
-# https://www.hackerrank.com/challenges/equality-in-a-array/problem?h_r=profile
-
-from collections import  Counter
-def equalizeArray(arr):
-    c = Counter(arr)
-    return len(arr) - max(c.values())
